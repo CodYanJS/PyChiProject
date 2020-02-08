aae20fea3aa09055786c07f2cc0d6afce78359b5
CodEarth <bbyanis2008@hotmail.fr>
2020-02-08 17:19:26 +0100

    2

--- sketch_feb08a.ino
+++ sketch_feb08a.ino
@@ -8,23 +8,46 @@
 /* Créer un objet Servo pour contrôler le servomoteur */
 Servo monServomoteur;
 
+int redpin = 11; //select the pin for the red LED
+int bluepin =10; // select the pin for the  blue LED
+int greenpin =9;// select the pin for the green LED
+ 
+int val;
+ 
 void setup() {
-    
+      pinMode(redpin, OUTPUT);
+  pinMode(bluepin, OUTPUT);
+  pinMode(greenpin, OUTPUT);
+  Serial.begin(9600);
   // Attache le servomoteur à la broche D9
-  /*monServomoteur.attach(9);*/
+  monServomoteur.attach(9);
 }
 
 void loop() {
-
+  for(val=255; val>0; val--)
+  {
+   analogWrite(11, 255);
+   analogWrite(10, 0-val);
+   analogWrite(9, 0-val);
+   delay(1); 
+  }
+for(val=0; val<255; val++)
+  {
+   analogWrite(11, 255);
+   analogWrite(10, 0-val);
+   analogWrite(9, 0-val);
+   delay(1); 
+  }
+ Serial.println(val, DEC);
   // Fait bouger le bras de 0° à 180°
   for (int position = 0; position <= 180; position++) {
     monServomoteur.write(position);
-    delay(20);
+    delay(15);
   }
   
   // Fait bouger le bras de 180° à 10°
   for (int position = 180; position >= 0; position--) {
     monServomoteur.write(position);
-    delay(20);
+    delay(15);
   }
 }
